eac9568bcefb4957eafc7d5b72ec3497946bb9eb
Matthew Franglen <matthew@franglen.org>
2018-01-30 22:35:05 +0000

    Delete old code

--- chunk.py
+++ chunk.py
@@ -1,6 +1,6 @@
 """ Chunks a file into a bunch of files based on a delimiter and a size limit """
 
-from io import BytesIO, RawIOBase
+from io import RawIOBase
 
 DEFAULT_LIMIT = 1024 * 1024
 DEFAULT_BUFFER_SIZE = 4 * 1024
@@ -30,32 +30,6 @@ def chunk(
         )
         yield current
         last = current
-
-    remainder = b''
-    while True:
-        buffer = _read(source, remainder, limit)
-        if not buffer:
-            return
-
-        if _is_within_limit(buffer, limit):
-            yield BytesIO(buffer)
-        else:
-            data, remainder = _partition(buffer, delimiter)
-            yield BytesIO(data)
-
-def _read(source, remainder, limit):
-    size = limit - len(remainder)
-    buffer = source.read(size)
-    return remainder + buffer
-
-def _is_within_limit(buffer, limit):
-    return len(buffer) < limit
-
-def _partition(buffer, delimiter):
-    data, delim, remainder = buffer.rpartition(delimiter)
-    if not delim:
-        raise ValueError('No delimiter found within chunk')
-    return data + delim, remainder
 
 class LimitedReader(RawIOBase):
 
